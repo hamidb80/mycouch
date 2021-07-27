7ab3c247e8098865f1d6ca37ff5a664d28c33b8b
hamidb80 <hr.bolouri@gmail.com>
2021-07-27 12:27:43 +0430

    ready for test

--- src/mycouch/queryServer/designDocuments.nim
+++ src/mycouch/queryServer/designDocuments.nim
@@ -1,7 +1,8 @@
 ## this file is based on following link from CouchDB documentations:
 ## https://docs.couchdb.org/en/latest/ddocs/ddocs.html#design-documents
 
-import json, tables
+import macros, json, tables
+import macroutils except name
 
 
 type
@@ -33,3 +34,86 @@ proc log*(msg: string) =
 
 template emit*(key: JsonNode, val = newJNull()) =
   result.add( %* [key, val])
+
+# ----------------------------------------------
+
+type
+  FuncStore[Fn] = Table[string, Fn]
+
+
+var
+  mapFuncs*: FuncStore[MapFun]
+  reduceFuncs*: FuncStore[ReduceFun]
+  rereduceFuncs*: FuncStore[RereduceFun]
+  updateFuncs*: FuncStore[UpdateFun]
+  filterFuncs*: FuncStore[Filterfun]
+  validateFuncs*: FuncStore[ValidateFun]
+
+
+template prepare {.dirty.}=
+  expectKind body, {nnkProcDef, nnkFuncDef}
+  let fname = body.name
+
+template patternError {.dirty.} =
+  error "pattern mismatch"
+  
+
+macro mapfun*(body)=
+  prepare
+  
+  superQuote:
+    `body`
+    when `fname` is MapFun:
+      mapFuncs.add `fname.strval`, `fname`
+    else:
+      patternError
+
+macro redfun*(body)=
+  prepare
+  
+  superQuote:
+    `body`
+    when `fname` is ReduceFun:
+      reduceFuncs.add `fname.strval`, `fname`
+    else:
+      patternError
+
+macro reredfun*(body)=
+  prepare
+  
+  superQuote:
+    `body`
+    when `fname` is RereduceFun:
+      rereduceFuncs.add `fname.strval`, `fname`
+    else:
+      patternError
+
+macro updatefun*(body)=
+  prepare
+  
+  superQuote:
+    `body`
+    when `fname` is UpdateFun:
+      updateFuncs.add `fname.strval`, `fname`
+    else:
+      patternError
+
+macro filterfun*(body)=
+  prepare
+  
+  superQuote:
+    `body`
+    when `fname` is Filterfun:
+      filterFuncs.add `fname.strval`, `fname`
+    else:
+      patternError
+
+macro validatefun*(body)=
+  prepare
+  
+  superQuote:
+    `body`
+    when `fname` is ValidateFun:
+      validateFuncs.add `fname.strval`, `fname`
+    else:
+      patternError
--- src/mycouch/queryServer/protocol.nim
+++ src/mycouch/queryServer/protocol.nim
@@ -1,65 +1,111 @@
 ## https://docs.couchdb.org/en/latest/query-server/protocol.html
 
-import json, sequtils, strformat
+import json, sequtils, tables, strformat
 import designDocuments
 
-var
-  mapFunc: MapFun
-  reduceFunc: ReduceFun
-  rereduceFunc: RereduceFun
-  updateFunc: UpdateFun
-  filterFunc: Filterfun
-  validateFunc: ValidateFun
+
+template throw(msg: string)=
+  raise newException(ValueError, msg)
+
+template checkFuncExistance(name: string, collection: untyped)=
+  if name notin collection:
+    throw "func with name '" & name & "' does not exist"
+
+type
+  Ddoc = object
+    updates: Table[string, string]
+    filters: Table[string, string]
+    mapper: string
+    validator: string
+
+var 
+  selectedMapFunc: MapFun
+  ddocs: Table[string, Ddoc]
 
 proc dataProcessor(command: string, args: seq[JsonNode]): JsonNode =
   case command:
   of "reset":
+    selectedMapFunc = nil
+    % true
+
+  of "add_fun":
+    let fname = args[1].str
+    checkFuncExistance fname, mapFuncs
+    
+    selectedMapFunc = mapFuncs[fname]
     % true
 
   of "map_doc":
     let doc = args[1]
-    %* [doc.mapFunc]
+    %* [selectedMapFunc(doc)]
 
   of "reduce":
-    let docs = args[1].getElems
+    let
+      fname = args[0][0].str 
+      docs = args[1].getElems
 
-    %* [true, [docs.reduceFunc]]
+    %* [true, [reduceFuncs[fname](docs)]]
 
   of "rereduce":
-    let values = args[1].getElems
+    let
+      fname = args[0][0].str 
+      values = args[1].getElems
 
-    %* [true, [values.rereduceFunc]]
+    %* [true, [rereduceFuncs[fname](values)]]
 
   of "ddoc":
-    if args[0] == %"new": return % true
+    if args[0] == %"new": 
+      let 
+        ddocName = args[1].str
+        ddocObj = args[2]
+      
+      ddocs[ddocname] = Ddoc()
+
+      if "filters" in ddocObj:
+        for name, src in ddocObj["filters"]:
+          ddocs[ddocname].filters.add name, src.str
+
+      if "updates" in ddocObj:
+        for name, src in ddocObj["updates"]:
+          ddocs[ddocname].updates.add name, src.str
+
+      ddocs[ddocname].mapper = ddocObj.getOrDefault("view").getOrDefault("map").getStr("")
+      ddocs[ddocname].validator = ddocObj.getOrDefault("validate_doc_update").getStr("")
+
+      return % true
 
     let 
-      id = args[0].str
+      ddocid = args[0].str
       subCommand = args[1][0].str
-      funcname = args[1][1].str
+      funcname = 
+        if args[1].len == 2: args[1][1].str
+        else: ""
       myArgs = args[2].getElems
 
     case subCommand:
 
     of "updates":
-      let updated = updateFunc(myArgs[0], myArgs[1])
+      let updated = updateFuncs[ddocs[ddocid].updates[funcname]](myArgs[0], myArgs[1])
       %* ["up", updated.newDoc, updated.response]
 
     of "filters":
-      %* [true, myArgs[0].mapIt filterFunc(it, myArgs[1])]
+      let fn = filterFuncs[ddocs[ddocid].filters[funcname]]
+      %* [true, myArgs.mapIt fn(it, myArgs[1])]
 
     of "views":
-      let docs = args[2]
-      %* [true, myArgs[0].mapIt it.mapFunc.len != 0]
+      let fn = mapFuncs[ddocs[ddocid].mapper]
+      %* [true, myArgs.mapIt fn(it).len != 0]
 
     of "validate_doc_update":
       let
-        newDoc = args[0]
-        oldDoc = args[1]
-        req = args[2]
-        sec = args[3]
+        newDoc = myArgs[0]
+        oldDoc = myArgs[1]
+        req = myArgs[2]
+        sec = myArgs[3]
 
-      validateFunc(newDoc, oldDoc, req, sec)
+        fn = validateFuncs[funcname]
+      
+      fn(newDoc, oldDoc, req, sec)
       % 1
 
     else:
@@ -82,4 +128,4 @@ proc run*()=
       echo %*{"unauthorized": e.msg}
 
     except Exception as e:
-      echo (%*["error", $e.name, $e.msg])
+      echo %*["error", $e.name, $e.msg]
